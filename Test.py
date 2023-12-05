4b8e5bd8606bb29cc555136fe28cb7cc30d8a9db
sharlorogers <srogers@goddardusd.com>
2023-12-05 10:23:34 -0600

    1.0
    This is my version of the test file.

---
+++ Test.py
@@ -0,0 +1,8 @@
+#Sharlo Rogers
+#Test
+#December 4, 2023
+
+def main():
+    print("This is a test. It is only a test. If this had been a real program, you would have received further instructions on where to go and what to do.")
+    
+main()
