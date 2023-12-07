49304a1430b1a9fa3821042be03afb091e90ab30
sharlorogers <srogers@goddardusd.com>
2023-12-07 07:49:20 -0600

    Create Contact Manager Team Project.py

---
+++ Contact Manager Team Project.py
@@ -0,0 +1,54 @@
+#All of your names
+#Contact Manager Team Project
+#November 29 - December 4
+
+def main():
+    big loop
+        option = display_menu()  #returns a choice
+        if ______
+            add_contact()
+        elif______
+            info = search_contact()
+            print(info)
+        elif
+            edit_contact()
+        elif
+            delete_contact()
+        else
+            display_contacts()
+    
+#End of main()
+
+#PRE-Condition: What do I need to receive - Nothing
+#POST-Condition: What am I returning - RETURNS A CHOICE
+def display_menu():
+    #A function to display the menu and return the choice 
+    
+#PRE-Condition: What do I need to receive - NOTHING
+#POST-Condition: What am I returning - NOTHING
+def add_contact():
+    #An add contact function that will add a contact to the file
+    
+    
+#PRE-Condition: What do I need to receive - NOTHING
+#POST-Condition:What am I returning - RETURNS the contact info if found 
+def search_contact():
+    #A search contact function that will search the file for a contact and return the information for that contact, if found
+    
+    
+#PRE-Condition:
+#POST-Condition:
+def edit_contact():
+    #An edit contact function that will use the search contact function to find someone and change the information if found
+    
+    
+#PRE-Condition:
+#POST-Condition:
+def delete_contact():
+    #A delete contact function that will use the search contact function to find someone and delete the information, if found
+    
+    
+#PRE-Condition:
+#POST-Condition:
+def display_contacts():
+    #A display contacts function that displays all contacts and their information in a readable format
